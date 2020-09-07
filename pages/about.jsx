3a6045d576c073893898401ca152d2f42b8777a5
Aman <devproddeploy@gmail.com>
2020-09-08 00:20:36 +0530

    repo link

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,11 +1,17 @@
 const about = () => {
     return (
         <div className='container'>
-            <hr/>
-            <p className="flow-text">Full Stack Next.js, GraphQL Resume</p>
-            <p className="flow-text">
-                Next.js | GraphQL | Apollo | Nexus Schema | React Syntax Highlighter | Materialize
+            <div>
+                <p className="flow-text">Full Stack Next.js, GraphQL Resume</p>
+                <p className="flow-text">
+                    Next.js | GraphQL | Apollo | Nexus Schema | React Syntax Highlighter | Materialize
             </p>
+            </div>
+            <div className="center">
+                <a href="https://github.com/inblack67/FullStack-Graphql-Next.js-Resume" rel='noopener noreferrer' target='_blank'>
+                    <i className="fab fa-github fa-3x red-text"></i>
+                </a>
+            </div>
         </div>
     )
 }
